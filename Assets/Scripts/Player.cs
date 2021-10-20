4f33ede23dce96cb017e5363fc76055814275ccd
asdfghjklmum <chenqi63656@gmail.com>
2021-10-20 21:24:20 +0800

    Design a GameOver scene and Add in a GameOver text and add in Restart button
    When the user clicks the Restart button, the game should restart

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -14,11 +15,18 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.position = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
 
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -2.9f, 4.9f), transform.position.z);
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
